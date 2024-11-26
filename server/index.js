594482bafcd3cddf59d0ee90fa15a1648804a05d
Jatinkr07 <jatinkumar07911@gmail.com>
2024-11-26 14:34:46 +0530

    under build

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://helpful-lily-ccb1b9.netlify.app/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
